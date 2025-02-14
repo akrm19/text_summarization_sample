8bc6a221cff6b2055d7a9287ec930c939c386ce3
akrm19 <37549312+akrm19@users.noreply.github.com>
2025-02-13 23:13:23 -0600

    more updates

--- src/map_reduce_summarizer/graph.py
+++ src/map_reduce_summarizer/graph.py
@@ -12,13 +12,14 @@ from langgraph.constants import Send
 from langgraph.graph import END, START, StateGraph
 from langchain.chains.combine_documents.reduce import (
     acollapse_docs,
+    collapse_docs,
     split_list_of_docs,
 )
 
 from map_reduce_summarizer.state import SummaryState, OverallState
 from text_utils import get_docs_from_url, split_text
 
-llm = ChatOllama(model="gpt2", temperature=0)
+llm = ChatOllama(model="llama3.2", temperature=0)
 token_max = 1000
 
 # Prompts
@@ -43,9 +44,9 @@ def length_function(documents: List[Document]) -> int:
     """Get the number of tokens for input contents"""
     return sum(llm.get_num_tokens(doc.page_content) for doc in documents)
 
-async def _reduce(input: dict) -> str:
+def _reduce(input: dict) -> str:
     prompt = reduce_prompt.invoke(input)
-    response = await llm.ainvoke(prompt)
+    response = llm.invoke(prompt)
     return response.content
 
 # Graph functions
@@ -67,21 +68,23 @@ def map_summaries(state: OverallState):
 
     docs = get_docs_from_url(state.url)  #(state["url"])
     split_docs = split_text(docs)
-    state.contents = [doc.page_content for doc in split_docs]
+    
+    retrieved_content = [doc.page_content for doc in split_docs]
+    state.contents = retrieved_content
 
     return [
-         Send("generate_summary", {"content": content}) for content in state.contents 
+         Send("generate_summary", {"content": content}) for content in retrieved_content 
         #Send("generate_summary", SummaryState(content=content)) for content in state.contents # {"content": content}) for content in state.contents # state["contents"]
     ]
     #return [SummaryState(content=content) for content in state["contents"]]
     # for content in state["contents"]:
     #     yield SummaryState(content=content)
 
-async def generate_summary(state: SummaryState):
+def generate_summary(state: SummaryState):
     """Generate a summary for a given content"""
 
-    prompt = map_prompt.invoke(state.content) #(state["content"])
-    response = await llm.ainvoke(prompt)
+    prompt = map_prompt.invoke(state["content"]) #state.content) #(state["content"])
+    response = llm.invoke(prompt)
     return {"summaries": [response.content]}
 
 
@@ -98,18 +101,18 @@ def should_collapse(state: OverallState) -> Literal["collapse_summaries", "gener
     else:
         return "generate_final_summary"
 
-async def collapse_summaries(state: OverallState):
+def collapse_summaries(state: OverallState):
     """Collapse the summaries into a final summary"""
     doc_lists = split_list_of_docs(state.collapsed_summaries, length_function, token_max) #(state["collapsed_summaries"], length_function, token_max)
     results = []
     for doc_list in doc_lists:
-        results.append(await acollapse_docs(doc_list, _reduce))
+        results.append(collapse_docs(doc_list, _reduce))
 
     return {"collapsed_summaries": results}
     
-async def generate_final_summary(state: OverallState):
+def generate_final_summary(state: OverallState):
     """Generate the final summary"""
-    response = await _reduce(state.collapsed_summaries) #(state["collapsed_summaries"])
+    response = _reduce(state.collapsed_summaries) #(state["collapsed_summaries"])
     return {"final_summary": response}
 
 # Graph definition
