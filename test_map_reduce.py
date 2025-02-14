5dfee66ae602cc1c9374d1156efb637733978405
akrm19 <37549312+akrm19@users.noreply.github.com>
2025-02-13 23:58:40 -0600

    more

--- test_map_reduce.py
+++ test_map_reduce.py
@@ -1,5 +1,6 @@
 from src.map_reduce_summarizer.state import OverallState, SummaryState
 from src.map_reduce_summarizer.graph import map_reduce_graph
+from src.text_utils import split_text, get_docs_from_url
 
 
 summary_state = SummaryState(content="test")
@@ -7,4 +8,14 @@ print(summary_state.content)
 
 starting_state = OverallState(url="https://langchain-ai.github.io/langgraph/troubleshooting/errors/")
 print(starting_state.url)
+#
+
+docs = get_docs_from_url(starting_state.url)  #(state["url"])
+split_docs = split_text(docs)
+
+retrieved_content = [doc.page_content for doc in split_docs]
+
+print(len(retrieved_content))
 summary = map_reduce_graph.invoke(starting_state)
+
+print("done")
